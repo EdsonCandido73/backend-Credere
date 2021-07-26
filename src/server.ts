9d11b56662d89dd8615dc9e6dd96ba3b777830bd
Edson Candido <edson.cursoprogramacao@gmail.com>
2021-07-26 20:45:16 -0300

    Deploy Heroku

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 app.use( async(req, res, next) => {
   const coordinatesRepository = getRepository(Coordinates);
   const coordinates = await coordinatesRepository.findOne(1);
